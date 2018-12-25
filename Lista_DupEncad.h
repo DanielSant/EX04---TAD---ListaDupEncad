565d92c643a13eb48fc4a9a77059f63743d6cb43
Daniel Sants <danielsantos619@yahoo.com.br>
2018-12-25 11:24:27 -0300

    Adicionada estrutura (structs) da lista duplamente encadeada

--- Lista_DupEncad.h
+++ Lista_DupEncad.h
@@ -1,6 +1,23 @@
 #ifndef LISTA_DUPENCAD_H_INCLUDED
 #define LISTA_DUPENCAD_H_INCLUDED
 
+typedef struct nodoLista *apontador;
 
+typedef struct {
+    int chave;
+    /*outros componentes*/
+} TInfo;
+
+struct nodoLista
+{
+    TInfo item;
+    struct nodoLista *anterior;
+    struct nodoLista *proximo;
+};
+
+typedef struct {
+    struct nodoLista *primeiro;
+    struct nodoLista *ultimo;
+} TLista;
 
 #endif // LISTA_DUPENCAD_H_INCLUDED
